21697e87fa3824b3a75f4096861bc58a53b7c6ff
Amith Sai <amith@Amiths-MacBook-Air.local>
2026-01-31 22:21:58 +0530

    fix: fixed output value

--- terraform/modules/services/output.tf
+++ terraform/modules/services/output.tf
@@ -1,4 +1,4 @@
 output "trancoder_service_name" {
   description = "The name of the Cloud Run service used for transcoding."
-  value       = google_cloud_run_service.trancoder_service.name
+  value       = google_cloud_run_service.apex_cr_start_transcode.name
 }
