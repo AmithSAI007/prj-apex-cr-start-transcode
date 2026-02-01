214f8450b4f835e56095f2f5018371f8d788ba95
Amith Sai <amith@Amiths-Air.lan>
2026-02-01 10:27:08 +0530

    fix: added pubsub publisher role

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,3 +1,18 @@
 data "google_service_account" "eventarc_sa" {
   account_id = var.service_account_name
 }
+
+data "google_projects" "project" {
+  filter = "projectId:${var.project_id}"
+}
+
+locals {
+  project_number = data.google_projects.project.projects[0].project_number
+  gcs_sa_email   = "service-${local.project_number}@gcp-sa-cloud-storage.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "gcs_pubsub_publisher" {
+  project = var.project_id
+  role    = "roles/pubsub.publisher"
+  member  = "serviceAccount:${local.gcs_sa_email}"
+}
